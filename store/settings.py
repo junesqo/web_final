d312e2342e7803e76e93b31b7048404f107d575d
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2023-05-08 13:43:43 +0600

    postgres db added

--- store/settings.py
+++ store/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webfinaldb',
+        'USER': 'postgres',
+        'PASSWORD': 'decode80',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
